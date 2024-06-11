503daef56de756fdc1fef078842d95ec97811205
omoiFly <omoifly@hotmail.com>
2024-06-11 15:15:03 +0000

    optimize pattern matching, add test

--- compiler.rkt
+++ compiler.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require racket/set racket/stream)
 (require racket/fixnum)
+(require "interp.rkt")
 (require "interp-Lint.rkt")
 (require "interp-Lvar.rkt")
 (require "interp-Cvar.rkt")
@@ -79,40 +80,40 @@
   (match e
     [(Var x) (values (Var x) '())]
     [(Int x) (values (Int x) '())]
-    [(Let x rhs body) 
-      (define new-rhs (rco-exp rhs))
-      (define-values (new-body body-ss) (rco-atom body))
-      (values new-body (append `((,x . ,new-rhs)) body-ss))]
-    [(Prim op es) 
-      (define-values (new-es sss) (for/lists (l1 l2) ([e es]) (rco-atom e)))
-      (define ss (append* sss))
-      (define tmp (gensym 'tmp))
-      (values (Var tmp) (append ss `((,tmp . ,(Prim op new-es)))))]))
+    [(Let x rhs body)
+     (define new-rhs (rco-exp rhs))
+     (define-values (new-body body-ss) (rco-atom body))
+     (values new-body (append `((,x . ,new-rhs)) body-ss))]
+    [(Prim op es)
+     (define-values (new-es sss) (for/lists (l1 l2) ([e es]) (rco-atom e)))
+     (define ss (append* sss))
+     (define tmp (gensym 'tmp))
+     (values (Var tmp) (append ss `((,tmp . ,(Prim op new-es)))))]))
 
 (define (rco-exp e)
   (match e
     [(Var x) (Var x)]
     [(Int n) (Int n)]
     [(Let x e body) (Let x (rco-exp e) (rco-exp body))]
-    [(Prim op es) 
-      (define-values (new-es sss) (for/lists (l1 l2) ([e es]) (rco-atom e)))
-      (define ss (append* sss))
-      (make-lets ss (Prim op new-es))]))
+    [(Prim op es)
+     (define-values (new-es sss) (for/lists (l1 l2) ([e es]) (rco-atom e)))
+     (define ss (append* sss))
+     (make-lets ss (Prim op new-es))]))
 
 
 (define (remove-complex-opera* p)
   (match p
-      [(Program info e) (Program info (rco-exp e))]))
+    [(Program info e) (Program info (rco-exp e))]))
 
 ;; explicate-control : Lvar^mon -> Cvar
 (define (explicate-tail e)
   (match e
     [(Var x) (Return (Var x))]
     [(Int n) (Return (Int n))]
-    [(Let x rhs body) 
-      (Seq 
-        (Assign (Var x) rhs)
-        (explicate-tail body))]
+    [(Let x rhs body)
+     (Seq
+      (Assign (Var x) rhs) ; assume here rhs is already atom, orelse we need extra explicate-assign
+      (explicate-tail body))]
     [(Prim op es) (Return (Prim op es))]
     [else (error "explicate_tail unhandled case" e)]))
 
@@ -125,11 +126,18 @@
 ;     [else (error "explicate_assign unhandled case" e)]))
 (define (explicate-control p)
   (match p
-    [(Program info body) (CProgram info (dict-set* '() 'start (explicate-tail body)))]))
+    [(Program info body) (CProgram info `((start . ,(explicate-tail body))))]))
+
+(define (instr-translation ast)
+  (match ast 
+    [(Seq s t) s]))
 
 ;; select-instructions : Cvar -> x86var
-(define (select-instructions p)
-  (error "TODO: code goes here (select-instructions)"))
+(define (select-instructions ast)
+  (match ast
+    [(CProgram info blocks)
+     (define start (dict-ref blocks 'start))
+     (instr-translation start)]))
 
 ;; assign-homes : x86var -> x86var
 (define (assign-homes p)
@@ -152,7 +160,7 @@
     ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
     ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
     ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
-    ;; ("instruction selection" ,select-instructions ,interp-x86-0)
+    ("instruction selection" ,select-instructions ,interp-x86-0)
     ;; ("assign homes" ,assign-homes ,interp-x86-0)
     ;; ("patch instructions" ,patch-instructions ,interp-x86-0)
     ;; ("prelude-and-conclusion" ,prelude-and-conclusion ,interp-x86-0)
